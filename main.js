a83b1d7e81df24ffba55b76f9b0413141699cdbc
dayrapachon <123913850+dayrapachon@users.noreply.github.com>
2023-08-07 13:01:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,37 @@ if ( age > 18 && registeredEarly === true) {
     console.log(`You will race at 12:30 pm with race number: ${raceNumber}`);
 }
 
+
+
+
+OTHER
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let registeredEarly = true;
+let runnersAge = 25;
+
+if (runnersAge >= 18 && registeredEarly === true) {
+  raceNumber = 1000;
+}
+
+if (runnersAge >= 18 && registeredEarly === true) {
+  console.log(`Race starts at 9:30 am, your race number is ${raceNumber}`)
+}
+
+if (runnersAge >= 18 && registeredEarly === false) {
+  raceNumber < 1000;
+}
+
+if (runnersAge >= 18 && registeredEarly === false) {
+  console.log(`Race starts at 11:00 am, your race number is ${raceNumber}`)
+}
+
+if (runnersAge < 18) {
+  raceNumber < 1000
+}
+
+if (runnersAge < 18) {
+  console.log(`Race starts at 12:30 pm, your race number is ${raceNumber}`)
+}
+
